8921d954ee28c697f199537a0bc1c6fc277886b2
Sxkshm <135103967+Sxkshm@users.noreply.github.com>
2023-06-01 12:34:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("This is an example");
+  res.send("This is a new example");
 });
 
 app.get("/info", (req, res) => {
